427ce74c9c605ce4ccd2515dc8b810d98519852d
Samnang MA <masamnang6@gmail.com>
2023-06-28 08:47:20 +0700

    React Redux Toolkit

---
+++ src/app/layout.js
@@ -0,0 +1,27 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ProviderRedux from "@/redux/ProviderRedux";
+import Navbar from "@/components/Navbar";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Ecommerce Next.js",
+  description:
+    "Ecommerce Next.js is an example of a simple ecommerce built with Next.js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ProviderRedux>
+          <Navbar />
+          {children}
+        </ProviderRedux>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></Script>
+      </body>
+    </html>
+  );
+}
